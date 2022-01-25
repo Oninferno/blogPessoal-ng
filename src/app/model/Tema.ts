b0a8bbca79ddefee44fbbddfe6f729bd28053586
Augusto Cardoso Xavier <augustocx15@gmail.com>
2022-01-25 14:23:24 -0300

    Rotas de entrada e cadastro criadas

---
+++ src/app/model/Tema.ts
@@ -0,0 +1,7 @@
+import { Postagem } from "./Postagems"
+
+export class Tema{
+    public id: number
+    public descricao: string
+    public postagem: Postagem[]
+}
---
+++ src/app/model/User.ts
@@ -0,0 +1,11 @@
+import { Postagem } from "./Postagems"
+
+export class User {
+    public id: number
+    public nome:  string
+    public foto: string
+    public usuario: string
+    public senha: string
+    public tipo: string
+    public postagem: Postagem[]
+}
